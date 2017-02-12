b37aed3c79d8e4f5dc6df4afa58c0b7d6534f267
vzaidman <vzaidman@gmail.com>
2017-02-12 21:15:27 +0300

    added a test project

---
+++ test-project/src/App.js
@@ -0,0 +1,53 @@
+import React, { Component, PropTypes } from 'react'
+import createPureStatelessComponent from '../../index'
+import './App.css'
+
+const SomeChildComponent = createPureStatelessComponent({
+  displayName: 'MyStatelessComponent',
+  propTypes: {
+    value: PropTypes.number.isRequired,
+    handleClick: PropTypes.func.isRequired
+  },
+  render({ value, handleClick }) {
+    const onClick = e => {
+      e.preventDefault()
+      handleClick(value)
+    }
+    return (
+      <div onClick={onClick} className='simple-button'>
+        {`Click Here ${value}`}
+      </div>
+    );
+  }
+})
+
+class App extends Component {
+  state = {
+    clickedDivs: ''
+  }
+
+  handleClick = (value) => {
+    const {clickedDivs} = this.state
+    const separator = !clickedDivs ? '' : ', '
+    this.setState({clickedDivs: clickedDivs + separator + value})
+  }
+
+  render() {
+    const {clickedDivs} = this.state
+
+    return (
+      <div>
+        {['a', 'b', 'c'].map((value, index) =>
+          <SomeChildComponent
+            key={index}
+            value={value}
+            handleClick={this.handleClick}
+          />
+        )}
+        <div key="clicked-divs">{clickedDivs}</div>
+      </div>
+    )
+  }
+}
+
+export default App;
