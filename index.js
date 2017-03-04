06908990beae81e8d07cf54b68255fbf87bb8e99
vzaidman <vzaidman@gmail.com>
2017-03-04 09:48:01 +0200

    improved test syntax dir and added test with recompose

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var shallowEqual = require('fbjs/lib/shallowEqual')
 
 module.exports = function pureStateless(generator) {
   return createClass({
-    shouldComponentUpdate(nextProps) {
+    shouldComponentUpdate: function shouldComponentUpdate(nextProps) {
       return !shallowEqual(this.props, nextProps)
     },
-    componentWillMount(){
+    componentWillMount: function componentWillMount(){
       this.handlers = {}
       this.statelessRender = generator(this.handlers, this.props, this.context)
     },
-    render(){
+    render: function render(){
       return this.statelessRender(this.handlers, this.props, this.context)
     }
   })
